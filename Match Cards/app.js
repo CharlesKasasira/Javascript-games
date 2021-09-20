2e3f0d47c8307dc941fe9a520342f4aa5c1a0337
CharlesKasasira <charleskasasira01@gmail.com>
2021-09-20 14:52:12 +0300

    initial commit

---
+++ Match Cards/app.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cardArray = [
+        {
+            name: "one",
+            img: "images/1.png"
+        },
+        {
+            name: "two",
+            img: "images/2.png"
+        },
+        {
+            name: "three",
+            img: "images/3.png"
+        },
+        {
+            name: "four",
+            img: "images/4.png"
+        },
+        {
+            name: "five",
+            img: "images/5.png"
+        },
+        {
+            name: "six",
+            img: "images/6.png"
+        },
+        {
+            name: "cover",
+            img: "images/cover.png"
+        },
+        {
+            name: "blank",
+            img: "images/blank.png"
+        }
+    ]
+
+
+    
+})
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Javascript Games
+
+***by charles Kasasira***
